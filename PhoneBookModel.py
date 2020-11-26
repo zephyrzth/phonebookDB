897572fcf2ee0b0469690aa52a40312f1fc05714
royyana@gmail.com <royyana@gmail.com>
2020-11-26 12:15:48 +0700

    first project

---
+++ PhoneBookModel.py
@@ -0,0 +1,60 @@
+import shelve
+import uuid
+
+class PhoneBook:
+    def __init__(self):
+        self.namafile = 'phonebook.db'
+        self.db = shelve.open(self.namafile,writeback=True)
+    def list(self):
+        data = []
+        try:
+            for i in self.db.keys():
+                data.append(dict(id=i,data=self.db[i]))
+            return dict(status='OK',data=data)
+        except:
+            return dict(status='ERR',msg='Error')
+    def create(self,info):
+        try:
+            id = str(uuid.uuid1())
+            self.db[id] = info
+            return dict(status='OK',id=id)
+        except:
+            return dict(status='ERR',msg='Tidak bisa Create')
+    def delete(self,id):
+        try:
+            del self.db[id]
+            return dict(status='OK',msg='{} deleted' . format(id), id=id)
+        except:
+            return dict(status='ERR',msg='Tidak bisa Delete')
+    def update(self,id,info):
+        try:
+            self.db[id]=info
+            return dict(status='OK',msg='{} updated' . format(id), id=id)
+        except:
+            return dict(status='ERR',msg='Tidak bisa Update')
+    def read(self,id):
+        try:
+            return dict(status='OK',id=id,data=self.db[id])
+        except:
+            return dict(status='ERR',msg='Tidak Ketemu')
+
+
+
+
+
+if __name__=='__main__':
+    pd = PhoneBook()
+#    ----------- create
+#    result = pd.create(dict(nama='royyana',alamat='ketintang',notelp='6212345'))
+#    print(result)
+#    result = pd.create(dict(nama='ibrahim',alamat='ketintang',notelp='6212341'))
+#    print(result)
+#    result = pd.create(dict(nama='Ananda', alamat='Dinoyo Sekolahan', notelp='6212345'))
+#    print(result)
+#    ------------ list
+    print(pd.list())
+#    ------------ info
+#    print(pd.read('c516b780-2fa2-11eb-bf35-7fc0bd24c845'))
+
+
+
---
+++ README.txt
@@ -0,0 +1,7 @@
+gunakan python3.8 atau yang lebih baru
+buatlah dulu lingkungan virtual environment
+1. virtualenv venv
+2. source venv/bin/activate
+3. anda sudah berada dalam linkungan virtualenv
+4. jalankan instalasi modul pip install -r requirements
+
